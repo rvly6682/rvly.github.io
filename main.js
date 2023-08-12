0f20cf20974c138bd0b204e07a18f80fcd1dd067
rvly6682 <141377518+rvly6682@users.noreply.github.com>
2023-08-12 12:18:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+/*=== Typed Reveal ===*/
+
+
+
+
+
+const typed = new Typed('.text', {
+strings: ['Artist','Student','Web Development'],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 1000,
+loop: true
+});
+
+/*=== end ===*/
